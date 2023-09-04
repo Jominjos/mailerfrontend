296d745dc39eda02fa4c1f91ce4367ba1605d977
jominjos <sjominjos@gmail.com>
2023-09-04 16:14:26 +0530

    del user changes

--- src/components/deleteuser.js
+++ src/components/deleteuser.js
@@ -1,12 +1,17 @@
 import { useState } from "react";
 
 export default function Deleteuser() {
-  const [delUser, setdelUser] = useState({});
+  const [delUser, setdelUser] = useState({ name: "" });
   function OnDelete(event) {
     event.preventDefault();
     console.log(`ondel`);
   }
-  function delUserChange() {}
+  function delUserChange(event) {
+    event.preventDefault();
+    let deluser = event.target.value;
+    setdelUser({ name: deluser });
+    console.log(delUser);
+  }
   return (
     <>
       <h1>Delete User</h1>
