cab49587192f1500b82fa41ea3aa5ae207f4cf6a
satheesh <90098687+satheeshpsr47@users.noreply.github.com>
2023-09-05 16:00:37 +0530

    added DB to UI

--- src/components/userdb.js
+++ src/components/userdb.js
@@ -1,17 +1,44 @@
 import axios from "axios";
+
 import { useEffect, useState } from "react";
 
 export default function Dbtable() {
   const [db, setDb] = useState("");
   const fetchInfo = () => {
-    return axios
+      axios
       .get("https://jodemailer.onrender.com/db")
       .then((response) => setDb(response.data));
   };
 
+  fetchInfo();
   useEffect(() => {
     fetchInfo();
+    // console.log(db)
   }, []);
-  let dbData = [...db];
-  return <></>;
+  // console.log(db)
+  return <table>
+    <thead>
+      <tr>
+        <th>
+          Name
+        </th>
+        <th>Mail id </th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+        db && db.length >0 &&
+        db.map((d,i)=>(
+          
+          <tr key={d._id}>
+            <td>{d.name}</td>
+            <td>{d.Email}</td>
+          </tr>
+          
+          
+          
+        ))
+      }
+    </tbody>
+  </table>;
 }
